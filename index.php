6d4780fc2f371db26687404038f2e6c23c0e8a50
jbernadas <jb3rns@gmail.com>
2019-11-09 21:52:41 -0800

    Added box-shadows and identity to sidebar-first

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ include("includes/header.php");
 ?>
     <div class="user-details container">
       <div class="row">
-        <div class="col-md-3">
+        <div class="sidebar-first id col-md-3">
           <a href="#"><img src="<?php echo $user['profile_pic']; ?>" alt="User picture"></a>
+          <?php echo $user[first_name] . " " . $user[last_name]; ?>
         </div>
         <div class="col-md-9"></div>
       </div>

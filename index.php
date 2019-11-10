2b6d22edbd6e90b5c0faa0117dfa71e1a459d9cd
jbernadas <jb3rns@gmail.com>
2019-11-09 22:11:20 -0800

    Converted full-name inside sidebar-first and darkened color of brand text

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ include("includes/header.php");
       <div class="row">
         <div class="sidebar-first id col-md-3">
           <a href="#"><img src="<?php echo $user['profile_pic']; ?>" alt="User picture"></a>
-          <?php echo $user[first_name] . " " . $user[last_name]; ?>
+          <?php echo '<a href="#">' . $user[first_name] . " " . $user[last_name] . '</a>'; ?><br />
+          Posts: 0<br />
+          Likes: 0
         </div>
         <div class="col-md-9"></div>
       </div>

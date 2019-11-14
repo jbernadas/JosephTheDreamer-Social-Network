1dd1758b9ff38e1db4f639d73cff2fdff8bc412e
jbernadas <jb3rns@gmail.com>
2019-11-14 12:30:36 -0800

    Added main-content area and textarea input submit

--- index.php
+++ index.php
@@ -4,13 +4,25 @@ include("includes/header.php");
 ?>
     <div class="user-details container">
       <div class="row">
-        <div class="sidebar-first id col-md-3">
-          <a href="#"><img src="<?php echo $user['profile_pic']; ?>" alt="User picture"></a>
-          <?php echo '<a href="#">' . $user[first_name] . " " . $user[last_name] . '</a>'; ?><br />
-          Posts: 0<br />
-          Likes: 0
+        <div class="col-md-3">
+          <div class="sidebar-first id">
+            <a href="#"><img src="<?php echo $user['profile_pic']; ?>" alt="User picture"></a>
+            <?php echo '<a href="#">' . $user[first_name] . " " . $user[last_name] . '</a>'; ?><br />
+            <?php
+              echo "Posts: " . $user['num_posts'] . "<br />";
+              echo "Likes: " . $user['num_likes'];
+            ?>
+          </div>
         </div>
-        <div class="col-md-9"></div>
+        <div class="col-md-9">
+          <div class="main-column">
+            <form class="post-form" action="index.php" method="POST">
+              <textarea name="post_text" id="post-text" placeholder="Got something to say?"></textarea>
+              <input type="submit" name="post" id="post-button" value="Post">
+              <hr>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   </body>

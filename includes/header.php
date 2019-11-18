e8299ece0849860d6b0f4db88a2e21b65aad596a
jbernadas <jb3rns@gmail.com>
2019-11-18 08:50:34 -0800

    Added jquery menu item active to header, to dynamically add .active class to whichever menu item is active

--- includes/header.php
+++ includes/header.php
@@ -31,6 +31,9 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <!-- Main JS -->
+    <script src="assets/js/active-menu.js"></script>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -48,10 +51,10 @@
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
       </form>
       <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-        <li class="nav-item active">
-          <a class="nav-link" href="#" title="User"><?php echo $user['username']; ?><span class="sr-only">(current)</span></a>
-        <li class="nav-item active">
-          <a class="nav-link" href="index.php" title="Home"><i class="fas fa-home"></i><span class="sr-only">(current)</span></a>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo $userLoggedIn; ?>" title="User"><?php echo $user['username']; ?><span class="sr-only"></span></a>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php" title="Home"><i class="fas fa-home"></i><span class="sr-only"></span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#" title="Messages"><i class="fas fa-envelope"></i></a>

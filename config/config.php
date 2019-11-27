2cbbf57e42fe2f33a74e17f8f434afb29bdb6419
jbernadas <jb3rns@gmail.com>
2019-11-27 10:45:33 -0800

    Added .gitignore

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ session_start();
 
 $timezone = date_default_timezone_set("America/Los_Angeles");
 
-$con = mysqli_connect("localhost", "root", "", "facebook_clone"); // Connection variable
+$con = mysqli_connect("localhost", "jb", "alphRAlolonol", "facebook_clone"); // Connection variable
 
 if(mysqli_connect_errno()) {
   echo "Failed to connect: " . mysqli_connect_errno();

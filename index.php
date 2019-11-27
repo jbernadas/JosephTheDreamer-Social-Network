0a2c806529aef73b7e245f624848140aaef95616
jbernadas <jb3rns@gmail.com>
2019-11-27 14:24:15 -0800

    Added post creation from frontend to database

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 include("includes/header.php");
 include("includes/classes/User.php");
+include("includes/classes/Post.php");
+
+if(isset($_POST['post'])){
+  $post = new Post($con, $userLoggedIn);
+  $post->submitPost($_POST['post_text'], 'none');
+}
+
 ?>
     <div class="user-details container">
       <div class="row">

98a0f8acb93423c44e9d2b9170fcfe5cc4234225
jbernadas <jb3rns@gmail.com>
2019-12-02 10:42:14 -0800

    Commented out extra line of code that does not do anything (line 74)

--- index.php
+++ index.php
@@ -42,56 +42,56 @@ if(isset($_POST['post'])){
 
         loadPosts(); //Load first posts
 
-          $(window).scroll(function() {
-            var bottomElement = $(".status-post").last();
-            var noMorePosts = $('.posts-area').find('.noMorePosts').val();
+        $(window).scroll(function() {
+          var bottomElement = $(".status-post").last();
+          var noMorePosts = $('.posts-area').find('.noMorePosts').val();
 
-              // isElementInViewport uses getBoundingClientRect(), which requires the HTML DOM object, not the jQuery object. The jQuery equivalent is using [0] as shown below.
-              if (isElementInView(bottomElement[0]) && noMorePosts == 'false') {
-                  loadPosts();
-              }
-          });
-
-          function loadPosts() {
-              if(inProgress) { //If it is already in the process of loading some posts, just return
-            return;
-          }
-
-          inProgress = true;
-          $('#loading').show();
-
-          var page = $('.posts-area').find('.nextPage').val() || 1; //If .nextPage couldn't be found, it must not be on the page yet (it must be the first time loading posts), so use the value '1'
-
-          $.ajax({
-            url: "includes/handlers/ajax_load_posts.php",
-            type: "POST",
-            data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
-            cache:false,
-
-            success: function(response) {
-              $('.posts-area').find('.nextPage').remove(); //Removes current .nextpage
-              $('.posts-area').find('.noMorePosts').remove(); //Removes current .nextpage
-              $('.posts-area').find('.noMorePostsText').remove(); //Removes current .nextpage
-
-              $('#loading').hide();
-              $(".posts-area").append(response);
-
-              inProgress = false;
+            // isElementInViewport uses getBoundingClientRect(), which requires the HTML DOM object, not the jQuery object. The jQuery equivalent is using [0] as shown below.
+            if (isElementInView(bottomElement[0]) && noMorePosts == 'false') {
+                loadPosts();
             }
-          });
-          }
+        });
 
-          //Check if the element is in view
-          function isElementInView (el) {
-              var rect = el.getBoundingClientRect();
+        function loadPosts() {
+            if(inProgress) { //If it is already in the process of loading some posts, just return
+          return;
+        }
 
-              return (
-                  rect.top >= 0 &&
-                  rect.left >= 0 &&
-                  rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && //* or $(window).height()
-                  rect.right <= (window.innerWidth || document.documentElement.clientWidth) //* or $(window).width()
-              );
+        inProgress = true;
+        $('#loading').show();
+
+        var page = $('.posts-area').find('.nextPage').val() || 1; //If .nextPage couldn't be found, it must not be on the page yet (it must be the first time loading posts), so use the value '1'
+
+        $.ajax({
+          url: "includes/handlers/ajax_load_posts.php",
+          type: "POST",
+          data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+          cache:false,
+
+          success: function(response) {
+            $('.posts-area').find('.nextPage').remove(); //Removes current .nextpage
+            $('.posts-area').find('.noMorePosts').remove(); //Removes current .nextpage
+            // $('.posts-area').find('.noMorePostsText').remove(); //Removes current .nextpage
+
+            $('#loading').hide();
+            $(".posts-area").append(response);
+
+            inProgress = false;
           }
+        });
+        }
+
+        //Check if the element is in view
+        function isElementInView (el) {
+            var rect = el.getBoundingClientRect();
+
+            return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && //* or $(window).height()
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth) //* or $(window).width()
+            );
+        }
       });
 
     </script>

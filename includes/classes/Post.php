e05fe1f453c2c3f0d02d5484384f5c523af09089
jbernadas <jb3rns@gmail.com>
2019-12-08 11:12:18 -0800

    Styled comment area

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -110,6 +110,23 @@ class Post {
           $last_name = $user_row['last_name'];
           $profile_pic = $user_row['profile_pic'];
 
+          ?>
+
+          <script>
+            function toggle<?php echo $id; ?>() {
+              var element = document.getElementById("toggleComment<?php echo $id; ?>");
+
+              if(element.style.display == "block") {
+                element.style.display = "none";
+              }
+              else {
+                element.style.display = "block";
+              }
+            }
+          </script>
+
+          <?php
+
           // Timeframe
           $date_time_now = date("Y-m-d H:i:s");
           $start_date = new DateTime($date_time); // Time of post
@@ -175,7 +192,7 @@ class Post {
             }
           }
 
-          $str .= "<div class='status-post'>
+          $str .= "<div class='status-post' onClick='javascript:toggle$id()'>
                     <div class='post-profile-pic'>
                       <img src='$profile_pic' width='50'>
                     </div>
@@ -188,6 +205,9 @@ class Post {
                       $body<br>
                     </div>
 
+                  </div>
+                  <div class='post_comment' id='toggleComment$id' style='display: none'>
+                    <iframe src='comment_frame.php?post_id=$id' id='comment_iframe'></iframe>
                   </div>
                   <hr>";
         }
